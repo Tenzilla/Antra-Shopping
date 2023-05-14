d4168a67b501c0a0f0546877b847a4e5d93f63b7
Tenzin <tenzins97@gmail.com>
2023-05-14 16:51:41 -0400

    adding last touches that I realized I didn't save and commit

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
 const API = (() => {
   const URL = "http://localhost:3000";
 
-  const doAjax = async ( data, url, method ) => {
-    return new Promise(function(resolved, rejected){
-        const xhttp = new XMLHttpRequest();
-        xhttp.open(method, url);
-        xhttp.send(data);
-        xhttp.onreadystatechange = ()=>{
-            if (xhttp.readyState === 4) {
-                if (xhttp.status === 200) { resolved(xhttp.responseText); } 
-                else { rejected(xhttp.statusText); }
-            }
-        };
+  const doAjax = async (data, url, method) => {
+    return new Promise(function (resolved, rejected) {
+      const xhttp = new XMLHttpRequest();
+      xhttp.open(method, url);
+      xhttp.send(data);
+      xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === 4) {
+          if (xhttp.status === 200) { resolved(xhttp.responseText); }
+          else { rejected(xhttp.statusText); }
+        }
+      };
     });
   }
 
-  const getCart = () => {
-    // define your method to get cart data
+  const getCart = (view) => {
+    doAjax("", `http://127.0.0.1:4051/get-cart`, "GET").then(
+      res => {
+        let cartData = JSON.parse(res);
+
+        cart_item_cont_1 = document.createElement("div")
+        cart_item_cont_2 = document.createElement("div")
+
+        cart_item_cont_1.classList.add("each-cart")
+        cart_item_cont_2.classList.add("each-cart")
+
+        cart_item_cont_1.id = "apple-cart-cont"
+        cart_item_cont_2.id = "orange-cart-cont"
+
+        cart_item_cont_1.innerHTML = `Apple x ${cartData.data.apple} <button class="delete-from-cart" id="delete-apple-from-cart">delete</button>`
+        cart_item_cont_2.innerHTML = `Orange x ${cartData.data.orange} <button class="delete-from-cart" id="delete-orange-from-cart">delete</button>`
+
+        if (cartData.data.apple != 0) {
+          view.cart_container_El.append(cart_item_cont_1)
+        }
+        if (cartData.data.orange != 0) {
+          view.cart_container_El.append(cart_item_cont_2)
+        }
+      }
+    )
   };
 
-  const getInventory = () => {
-    // define your method to get inventory data
+  const getInventory = (view) => {
+    doAjax("", `http://127.0.0.1:4051/get-inventory`, "GET").then(
+      res => {
+        let inventoryData = JSON.parse(res);
+        //console.log(inventoryData) 
+      }
+    )
   };
 
   const addToCart = (inventoryItem) => {
-    // define your method to add an item to cart
+    const formData = new FormData();
+    formData.append('item', inventoryItem.item)
+    formData.append('count', inventoryItem.count)
 
-    const formData = new FormData(); 
-    formData.append( 'item', inventoryItem.item )
-    formData.append( 'count', inventoryItem.count )
-
-    doAjax( formData, "http://127.0.0.1:80/add-to-cart", "POST" ).then( 
-      res => { console.log(res) }
+    doAjax(formData, `http://127.0.0.1:4051/add-to-cart`, "POST").then(
+      res => {
+        window.location.reload()
+      }
     )
   };
 
@@ -39,14 +67,31 @@ const API = (() => {
     // define your method to update an item in cart
   };
 
-  const deleteFromCart = (id) => {
-    // define your method to delete an item in cart
+  const deleteFromCart = (item, view) => {
+    const formData = new FormData();
+    formData.append('item', item)
+
+    doAjax(formData, `http://127.0.0.1:4051/delete-from-cart`, "DELETE").then(
+      res => {
+
+        window.location.reload()
+
+        if (item == "Orange") {
+          view.orange_cart_cont_El.style.display = "none"
+        }
+        else if (item == "apple") {
+          view.apple_cart_cont_El.style.display = "none"
+        }
+      }
+    )
+
+
   };
 
   const checkout = () => {
     // you don't need to add anything here
     return getCart().then((data) =>
-      Promise.all(data.map((item) => deleteFromCart(item.id)))
+      Promise.all(data.map((item) => deleteFromCart("*")))
     );
   };
 
@@ -78,10 +123,10 @@ const Model = (() => {
       return this.#inventory;
     }
 
-    set cart(newCart) {}
-    set inventory(newInventory) {}
+    set cart(newCart) { }
+    set inventory(newInventory) { }
 
-    subscribe(cb) {}
+    subscribe(cb) { }
   }
   const {
     getCart,
@@ -117,43 +162,69 @@ const View = (() => {
   const apple_current_val_El = document.querySelector("#apple-current-val");
   const oranges_current_val_El = document.querySelector("#oranges-current-val");
 
-  //const _El = document.querySelector("");
+  const cart_container_El = document.querySelector("#cart-container");
 
-  return { 
-    add_apple_to_cart_El, 
-    add_orange_to_cart_El, 
-    
-    apple_current_val_El, 
+  const delete_apple_from_cart_El = document.querySelector("#delete-apple-from-cart");
+  const delete_orange_from_cart_El = document.querySelector("#delete-orange-from-cart");
+
+  const apple_cart_cont_El = document.querySelector("#apple-cart-cont");
+  const orange_cart_cont_El = document.querySelector("#orange-cart-cont");
+
+
+  return {
+    add_apple_to_cart_El,
+    add_orange_to_cart_El,
+
+    apple_current_val_El,
     oranges_current_val_El,
-    
+
     increment_apple_El, decrement_apple_El,
     increment_orange_El, decrement_orange_El,
+
+    cart_container_El,
+    delete_apple_from_cart_El,
+    delete_orange_from_cart_El,
+
+    apple_cart_cont_El,
+    orange_cart_cont_El,
   };
+
 })();
+
+
 
 const Controller = ((model, view) => {
   // implement your logic for Controller
   const state = new model.State();
 
-  const init = () => {};
+  const init = () => {
+
+    //--get cart
+    model.getCart(view)
+
+    //--get inventory
+    model.getInventory(view)
+
+  };
+
 
   const handleUpdateAmount = () => {
-    
+
     view.increment_apple_El.addEventListener("click", (event) => {
-        view.apple_current_val_El.value = parseInt(view.apple_current_val_El.value) + 1
+      view.apple_current_val_El.value = parseInt(view.apple_current_val_El.value) + 1
     })
 
     view.increment_orange_El.addEventListener("click", (event) => {
-        view.oranges_current_val_El.value = parseInt(view.oranges_current_val_El.value) + 1
+      view.oranges_current_val_El.value = parseInt(view.oranges_current_val_El.value) + 1
     })
 
 
     view.decrement_apple_El.addEventListener("click", (event) => {
-        view.apple_current_val_El.value = parseInt(view.apple_current_val_El.value) - 1
+      view.apple_current_val_El.value = parseInt(view.apple_current_val_El.value) - 1
     })
 
     view.decrement_orange_El.addEventListener("click", (event) => {
-        view.oranges_current_val_El.value = parseInt(view.oranges_current_val_El.value) - 1
+      view.oranges_current_val_El.value = parseInt(view.oranges_current_val_El.value) - 1
     })
 
 
@@ -163,33 +234,54 @@ const Controller = ((model, view) => {
   const handleAddToCart = () => {
 
     view.add_apple_to_cart_El.addEventListener("click", (event) => {
-        console.log("apple added to cart")
-        console.log(view.apple_current_val_El.value)
+      console.log("apple added to cart")
+      console.log(view.apple_current_val_El.value)
 
-        model.addToCart({ item: "Apple", count: parseInt(view.apple_current_val_El.value) }).then((data) => {
-          console.log(data)
-        })
+      model.addToCart({ item: "Apple", count: parseInt(view.apple_current_val_El.value) })
+      window.location.reload()
     })
 
     view.add_orange_to_cart_El.addEventListener("click", (event) => {
-        console.log("orange added to cart")
-        console.log(view.oranges_current_val_El.value)
+      console.log("orange added to cart")
+      console.log(view.oranges_current_val_El.value)
 
-        model.addToCart({ item: "Orange", count: parseInt(view.oranges_current_val_El.value) }).then((data) => {
-          console.log(data)
+      model.addToCart({ item: "Orange", count: view.oranges_current_val_El.value })
+      window.location.reload()
+
+    })
+
+
+  };
+
+  const handleDelete = () => {
+
+    window.addEventListener("load", function () {
+
+      if (document.querySelector("#delete-apple-from-cart")) {
+        document.querySelector("#delete-apple-from-cart").addEventListener("click", (event) => {
+          model.deleteFromCart("Apple", view)
+          window.location.reload()
         })
+      }
+
+      if (document.querySelector("#delete-orange-from-cart")) {
+        document.querySelector("#delete-orange-from-cart").addEventListener("click", (event) => {
+          model.deleteFromCart("Orange", view)
+          window.location.reload()
+        })
+      }
+
     })
 
   };
 
-  const handleDelete = () => {};
-
-  const handleCheckout = () => {};
+  const handleCheckout = () => { };
 
   const bootstrap = () => {
     init()
     handleUpdateAmount()
     handleAddToCart()
+    handleDelete()
   };
 
   return { bootstrap, };
