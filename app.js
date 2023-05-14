b7a2357ab256e89ee3f847547f90c1e29dc4bb18
Tenzin <tenzins97@gmail.com>
2023-05-14 16:41:45 -0400

    entering changes to delete

--- app.js
+++ app.js
@@ -9,55 +9,107 @@ var multerObj = multer();
 const app = express()
 
 var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200
+	origin: "http://127.0.0.1:8080",
+	optionsSuccessStatus: 200
 }
 
+var PORT = 4051;
+
+app.use(cors());
 app.use(express.json());
-app.use(multerObj.array()); 
+app.use(multerObj.array());
 app.use(express.static('public'));
 
 server = require('http').createServer(app);
 
-app.post('/add-to-cart', cors(corsOptions), function handler(req, res){
+app.get('/get-cart', cors(corsOptions), function handler(req, res) {
+	fs.readFile('db/db.json', 'utf8', function (err, data) {
+		let dbJSON = JSON.parse(data)
+		return res.json({ status: "success", data: dbJSON.db.cart });
+	})
+})
+
+
+app.get('/get-inventory', cors(corsOptions), function handler(req, res) {
+	fs.readFile('db/db.json', 'utf8', function (err, data) {
+		let dbJSON = JSON.parse(data)
+		return res.json({ status: "success", data: dbJSON.db.inventory });
+	})
+})
+
+
+app.post('/add-to-cart', cors(corsOptions), function handler(req, res) {
 
 
 	//--get the current data in the db
-	fs.readFile('db/db.json', 'utf8', function(err, data) {
-		let dbData = data;
+	fs.readFile('db/db.json', 'utf8', function (err, data) {
+		let dbJSON = JSON.parse(data)
+		dbJSON = dbJSON.db
 
-		let dbJSON = JSON.parse(dbData)
-		dbJSON = dbData.db
-		
 		let dbJSONCart = dbJSON.cart
 
-		if ( req.body.item == "Apple") {
-			dbJSONCart = {...dbJSONCart, ...{ apple: req.body.count, orange: dbJSON.cart.orange }}
+		if (req.body.item == "Apple") {
+			dbJSONCart = { ...dbJSONCart, ...{ apple: parseInt(dbJSON.cart.apple) + parseInt(req.body.count), orange: parseInt(dbJSON.cart.orange) } }
 		}
-		else if ( req.body.item == "Orange") {
-			dbJSONCart = {...dbJSONCart, ...{ orange: req.body.count, apple: dbJSON.cart.apple }}
+		else if (req.body.item == "Orange") {
+			dbJSONCart = { ...dbJSONCart, ...{ orange: parseInt(dbJSON.cart.orange) + parseInt(req.body.count), apple: parseInt(dbJSON.cart.apple) } }
 		}
 
 		//--now update db
-		db = {...dbJSON, ...{ cart: dbJSONCart, inventory: dbJSON.inventory }}
+		db = { ...dbJSON, ...{ cart: dbJSONCart, inventory: dbJSON.inventory } }
 
 		var dbText = JSON.stringify({ db });
 
 		fs.writeFileSync('db/db.json', dbText, err => {
-		    if(err){
-		    	return res.json({status: "error", msg: err})
-		    }
-		    else{
-					return res.json({status: "success", msg: "Cart updated!"});
-		    }
+			if (err) {
+				return res.json({ status: "error", msg: err })
+			}
+			else {
+				return res.json({ status: "success", msg: "Cart updated!" });
+			}
 		});
 
 	})
 
-	
+})
+
+
+app.delete('/delete-from-cart', cors(corsOptions), function handler(req, res) {
+
+	//--get the current data in the db
+	fs.readFile('db/db.json', 'utf8', function (err, data) {
+		let dbJSON = JSON.parse(data)
+		dbJSON = dbJSON.db
+
+		let dbJSONCart = dbJSON.cart
+
+		if (req.body.item == "Apple") {
+			dbJSONCart = { ...dbJSONCart, ...{ apple: 0, orange: dbJSON.cart.orange } }
+		}
+		else if (req.body.item == "Orange") {
+			dbJSONCart = { ...dbJSONCart, ...{ orange: 0, apple: dbJSON.cart.apple } }
+		}
+
+		//--now update db
+		db = { ...dbJSON, ...{ cart: dbJSONCart, inventory: dbJSON.inventory } }
+
+		var dbText = JSON.stringify({ db });
+
+		fs.writeFileSync('db/db.json', dbText, err => {
+			if (err) {
+				return res.json({ status: "error", msg: err })
+			}
+			else {
+				return res.json({ status: "success", msg: "Cart updated!" });
+			}
+		});
+
+	})
 
 })
 
-server.listen(80, function() {
-    console.log('ready to go!');
+
+
+server.listen(PORT, function () {
+	console.log('Server listening on PORT', PORT);
 });
